920d6e1b32df0eafa1dca729117a9d7e6f6e9f81
Anderson Escalante <amchipel@scl.edu.gt>
2025-07-08 10:11:22 -0600

    Add files via upload

---
+++ operadores.js
@@ -0,0 +1,16 @@
+let fecha = 2025;
+let edad2 = 16;
+
+// Fecha de nacimiento?
+let fechaNacimieto = fecha - edad2;
+console.log("resultado: ", fechaNacimieto);
+
+// 
+let num1 = 40;
+let num2 = 80;
+
+
+console.log("Suma: ", num1 + num2);
+console.log("Resta: ", num1 - num2);
+console.log("Multiplicacion: ", num1 *num2);
+console.log("Division: ", num1 / num2);
